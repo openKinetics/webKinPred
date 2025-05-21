4e7576cf38f7bf7be7a4badb6b489a63fbc7196c
Saleh <saleh.662000@gmail.com>
2025-05-21 18:49:53 +0100

    gitignore

---
+++ webKinPred/config_local.py
@@ -0,0 +1,20 @@
+PYTHON_PATHS = {
+    'DLKcat': '/home/saleh/anaconda3/envs/dlkcat_env/bin/python',
+    'EITLEM': '/home/saleh/anaconda3/envs/eitlem_env/bin/python',
+    'TurNup': '/home/saleh/anaconda3/envs/turnup_env/bin/python',
+    'UniKP': '/home/saleh/anaconda3/envs/unikp/bin/python',
+}
+
+PREDICTION_SCRIPTS = {
+    'DLKcat': '/home/saleh/webKinPred/api/DLKcat/DeeplearningApproach/Code/example/prediction_for_input.py',
+    'EITLEM': '/home/saleh/webKinPred/api/EITLEM/Code/eitlem_prediction_script.py',
+    'TurNup': '/home/saleh/webKinPred/api/TurNup/code/kcat_prediction.py',
+    'UniKP': '/home/saleh/webKinPred/api/UniKP-main/run_unikp.py',
+}
+
+TARGET_DBS = {"DLKcat/UniKP": "/home/saleh/webKinPred/fastas/dbs/targetdb_dlkcat",
+        "EITLEM": "/home/saleh/webKinPred/fastas/dbs/targetdb_EITLEM",
+        "TurNup": "/home/saleh/webKinPred/fastas/dbs/targetdb_turnup"
+}
+
+CONDA_PATH = "/home/saleh/anaconda3/bin/conda"
