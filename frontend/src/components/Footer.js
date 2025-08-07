2bde3daf0f87c7f8a99ff3d75c3077e884c81650
Saleh <saleh.662000@gmail.com>
2025-08-07 17:58:29 +0100

    footer + header stylers

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,5 +1,4 @@
 // src/components/Footer.js
-
 import React from 'react';
 import euLogo from '../assets/eu_logo.png';
 import reconLogo from '../assets/recon4imd_logo.png';
@@ -7,20 +6,33 @@ import reconLogo from '../assets/recon4imd_logo.png';
 function Footer() {
   return (
     <footer className="custom-footer">
-      <div className="footer-content">
-        <p className="footer-text">
-          KineticXPredictor
-        </p>
-        <p className="funding-statement">
-          Recon4IMD is co-funded by the European Union's Horizon Europe Framework Programme (101080997)
-        </p>
-        <div className="footer-logos">
-          <a href="https://www.recon4imd.org/" target="_blank" rel="noopener noreferrer">
-            <img src={reconLogo} alt="Recon4IMD Logo" className="footer-logo recon-logo" />
-          </a>
-          <a href="https://cordis.europa.eu/project/id/101080997" target="_blank" rel="noopener noreferrer">
-            <img src={euLogo} alt="European Union Logo" className="footer-logo eu-logo" />
-          </a>
+      <div className="container-fluid">
+        {/* A single row to align all content, stacking on small screens */}
+        <div className="row align-items-center text-center text-lg-start">
+
+          {/* Column 1: App Name and Copyright */}
+          <div className="col-lg-4 col-md-12 mb-3 mb-lg-0">
+            <p className="footer-brand mb-0">KineticXPredictor</p>
+            <p className="copyright-text mb-0">2025 University of Galway</p>
+          </div>
+
+          {/* Column 2: Funding Information and Logos */}
+          <div className="col-lg-8 col-md-12">
+            <div className="d-flex justify-content-center justify-content-lg-end align-items-center">
+              <p className="funding-text me-4 mb-0">
+                Co-funded by the European Union's Horizon Europe Framework Programme (101080997)
+              </p>
+              <div className="footer-logos">
+                <a href="https://www.recon4imd.org/" target="_blank" rel="noopener noreferrer">
+                  <img src={reconLogo} alt="Recon4IMD Logo" className="footer-logo" />
+                </a>
+                <a href="https://cordis.europa.eu/project/id/101080997" target="_blank" rel="noopener noreferrer">
+                  <img src={euLogo} alt="European Union Logo" className="footer-logo" />
+                </a>
+              </div>
+            </div>
+          </div>
+
         </div>
       </div>
     </footer>
