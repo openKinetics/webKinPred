b01e49bfc409745a2cc5cabc5ee8ad8bf032abe2
Saleh <saleh.662000@gmail.com>
2025-08-19 14:55:49 +0100

    remove console log

--- frontend/src/components/appClient.js
+++ frontend/src/components/appClient.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
 export async function ensureCsrfCookie(api = apiClient) {
-  console.log('Making call to ' + api.defaults.baseURL + '/csrf/');
   try { await api.get('/csrf/'); } catch (_) { /* swallow */ }
 }
 const apiClient = axios.create({
