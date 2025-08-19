3ad88147845f8ad9e5acda20c862ecfc8d8041a7
Saleh <saleh.662000@gmail.com>
2025-08-19 15:33:54 +0100

    django static url

--- webKinPred/urls.py
+++ webKinPred/urls.py
@@ -18,15 +18,20 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
+from django.urls import re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
 ]
 
-# Serve static files in production
-if not settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# Serve static files even in production
+urlpatterns += [
+    re_path(r'^django_static/(?P<path>.*)$', serve, {
+        'document_root': settings.STATIC_ROOT,
+    }),
+]
 
 # Serve media files
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
